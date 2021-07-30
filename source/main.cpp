459bd72501703b8ddb1a3c84b213a2183f6dd02e
Ivan Orfanidi <i.orfanidi@mail.ru>
2021-07-30 13:52:36 +0300

    Добавлен параметр дискретизации обработки номера фрейма.

--- source/main.cpp
+++ source/main.cpp
@@ -14,7 +14,7 @@ constexpr std::string_view NAME_MODEL_FILE = "bvlc_googlenet.caffemodel"; // Tra
 
 constexpr int WIDTH = 500;
 constexpr int HEIGHT = 500;
-constexpr int DELAY_MS = 10;
+constexpr int DELAY_MS = 1;
 
 void getLabelsFromFile(std::vector<std::string>& labels, const std::string& nameFile)
 {
@@ -36,13 +36,15 @@ int main(int argc, char* argv[])
 {
     std::string inputFile;
     std::string outputFile;
-    bool isCuda;
+    bool useCuda;
+    uint16_t frameNumber;
     boost::program_options::options_description desc("Options");
     desc.add_options()
         // All options:
         ("in,i", boost::program_options::value<std::string>(&inputFile)->default_value(""), "Path to input file.\n") //
         ("out,o", boost::program_options::value<std::string>(&outputFile)->default_value("output.mp4"), "Path to output file.\n") //
-        ("cuda,c", boost::program_options::value<bool>(&isCuda)->default_value(true), "Set CUDA Enable.\n") //
+        ("cuda,c", boost::program_options::value<bool>(&useCuda)->default_value(true), "Set CUDA Enable.\n") //
+        ("frame,f", boost::program_options::value<uint16_t>(&frameNumber)->default_value(1), "Set frame number.") //
         ("help,h", "Produce help message."); // Help
     boost::program_options::variables_map options;
     try {
@@ -87,12 +89,12 @@ int main(int argc, char* argv[])
     // Define the codec and create VideoWriter object.The output is stored in 'outcpp.avi' file.
     cv::VideoWriter video(outputFile, cv::VideoWriter::fourcc('m', 'p', '4', 'v'), fps, cv::Size(WIDTH, HEIGHT));
 
-    bool isCudaEnable = false;
+    bool cudaEnable = false;
     if (cv::cuda::getCudaEnabledDeviceCount() != 0) {
         cv::cuda::DeviceInfo deviceInfo;
-        if (deviceInfo.isCompatible() != 0 && isCuda) {
+        if (deviceInfo.isCompatible() != 0 && useCuda) {
             cv::cuda::printShortCudaDeviceInfo(cv::cuda::getDevice());
-            isCudaEnable = true;
+            cudaEnable = true;
         }
     }
 
@@ -100,10 +102,14 @@ int main(int argc, char* argv[])
     while (cv::waitKey(DELAY_MS) != ESCAPE_KEY) {
         // Read a new frame from video.
         cv::Mat source;
-        if (capture.read(source) == false) { // Breaking the while loop if the frames cannot be captured.
-            std::cerr << "Video camera is disconnected!" << std::endl;
-            return EXIT_FAILURE;
-        }
+        uint16_t i = 0;
+        do {
+            if (capture.read(source) == false) {
+                std::cerr << "Video camera is disconnected!" << std::endl;
+                return EXIT_FAILURE;
+            }
+            ++i;
+        } while (i < frameNumber);
 
         cv::dnn::Net neuralNetwork;
         // Read binary file and description file.
@@ -114,7 +120,7 @@ int main(int argc, char* argv[])
         }
 
         // Set CUDA as preferable backend and target
-        if (isCudaEnable) {
+        if (cudaEnable) {
             neuralNetwork.setPreferableBackend(cv::dnn::DNN_BACKEND_CUDA);
             neuralNetwork.setPreferableTarget(cv::dnn::DNN_TARGET_CUDA);
         }
@@ -151,7 +157,7 @@ int main(int argc, char* argv[])
 #else
         cv::putText(source, "in debug", cv::Point(180, source.size().height - 10), cv::FONT_HERSHEY_PLAIN, 1.1, cv::Scalar(0, 255, 0), 1, 5);
 #endif
-        if (isCudaEnable) {
+        if (cudaEnable) {
             cv::putText(source, "using GPUs", cv::Point(300, source.size().height - 10), cv::FONT_HERSHEY_PLAIN, 1.1, cv::Scalar(0, 255, 0), 1, 5);
         } else {
             cv::putText(source, "using CPUs", cv::Point(300, source.size().height - 10), cv::FONT_HERSHEY_PLAIN, 1.1, cv::Scalar(0, 255, 0), 1, 5);
